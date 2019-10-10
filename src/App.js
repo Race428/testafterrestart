8c00ef5f941f02fa8e8e9155d206dbd130a46384
Race <raceacheson@gmail.com>
2019-10-10 00:33:01 -0600

    add

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <afsdfdsf
+        sdfsdf
+        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
